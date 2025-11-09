06c6c2cf493c7a9123259d5f1b2e88a6585b0827
BrunoGT013 <brunogt1@al.insper.edu.br>
2025-11-09 18:40:48 -0300

    resto da class jogador

--- game.py
+++ game.py
@@ -16,3 +16,19 @@ class Jogador:
         # velocidade de movimento (px/s)
         self.speed = 300
 
+    def update(self, dt, keys):
+        #Atualiza posição horizontal do carro com base nas teclas.
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.rect.x -= self.speed * dt
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.rect.x += self.speed * dt
+        # impede sair da tela
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+
+    def draw(self, surface):
+        #desenha o carro
+        surface.blit(self.image, self.rect)
+
