747f288dfb57403347ea32fda83eceaaa2be013a
brunokb1 <brunokb1@al.insper.edu.br>
2025-11-10 17:51:34 -0300

    game over assets

--- assets.py
+++ assets.py
@@ -5,6 +5,7 @@ from config import IMG_DIR, SND_DIR, FNT_DIR, WIDTH, HEIGHT
 from config import PLAYER_WIDTH, PLAYER_HEIGHT, COIN_SIZE, OBSTACLE_WIDTH, OBSTACLE_HEIGHT, BOOST_SIZE
 
 # chaves dos assets
+# imagens
 BACKGROUND = 'background'
 PLAYER_IMG = 'player_img'
 MOEDA_IMG = 'moeda_img'
@@ -12,6 +13,8 @@ OBSTACULO_IMG = 'obstaculo_img'
 CONE_IMG = 'cone_img'
 BOOST_IMG = 'boost_img'
 LINHA_DE_CHEGADA_IMG = 'linha_de_chegada_img'
+TELA_INICIAL_IMG = 'tela_inicial_img'
+GAME_OVER_IMG = 'game_over_img'
 
 # sons
 MOEDA_SOUND = 'moeda_sound'
@@ -33,6 +36,8 @@ def load_assets():
     assets[CONE_IMG]   = pygame.image.load(os.path.join(IMG_DIR, 'cone.png')).convert_alpha()
     assets[BOOST_IMG]  = pygame.image.load(os.path.join(IMG_DIR, 'boost.png')).convert_alpha()
     assets[LINHA_DE_CHEGADA_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'finish_line.png')).convert_alpha()
+    assets[TELA_INICIAL_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'tela_inicial.png')).convert_alpha()
+    assets[GAME_OVER_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'game_over.png')).convert_alpha()
 
     # Escala 
     assets[BACKGROUND] = pygame.transform.scale(assets[BACKGROUND], (WIDTH, HEIGHT))
@@ -42,7 +47,10 @@ def load_assets():
     assets[CONE_IMG]   = pygame.transform.scale(assets[CONE_IMG], (int(OBSTACLE_WIDTH * 0.75), int(OBSTACLE_HEIGHT * 0.75)))
     assets[BOOST_IMG]  = pygame.transform.scale(assets[BOOST_IMG], (BOOST_SIZE, BOOST_SIZE))
     assets[LINHA_DE_CHEGADA_IMG] = pygame.transform.scale(assets[LINHA_DE_CHEGADA_IMG], (WIDTH, 80))
+    assets[TELA_INICIAL_IMG] = pygame.transform.scale(assets[TELA_INICIAL_IMG], (WIDTH, HEIGHT))
+    assets[GAME_OVER_IMG] = pygame.transform.scale(assets[GAME_OVER_IMG], (WIDTH, HEIGHT))
 
+    
     # Sons 
     assets[MOEDA_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'moeda.wav'))
     assets[CARRO_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'boost.wav'))
